1e04f864709deb6805f6ea43817e9fc95ac256b2
Knarquelya O'Neal <knarquelya.oneal@gmail.com>
2019-03-02 16:35:58 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,11 @@
 def using_include(array, element)
   using_include = ["wow", "I", "am", "really", "learning", "arrays!"]
   using_include.include?("wow")
-  using_include.include?("Tardis")
 end
+
+def using_include
+  using_include = ["wow", "I", "am", "really", "learning", "arrays!"]
+  using_include.include?("Tardis")
 
 def using_sort(array)
   using_sort = ["wow", "I", "am", "really", "learning", "arrays!"]
