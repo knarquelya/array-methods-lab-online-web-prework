0f950e9456dd54896903f026c1c6004e3c90ffde
Knarquelya O'Neal <knarquelya.oneal@gmail.com>
2019-03-02 16:22:30 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,5 @@
 def using_include(array, element)
-  using_include = ["hello", "world"]
+  using_include = ["wow", "I", "am", "really", "learning", "arrays!"]
   using_include.include?("hello")
   using_include = ["good", "bye"]
   using_include.include?("love")
