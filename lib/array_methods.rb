c8646890b982016aa9c72c08e687c8c72d9027fa
Knarquelya O'Neal <knarquelya.oneal@gmail.com>
2019-03-02 16:20:30 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,8 @@
 def using_include(array, element)
   using_include = ["hello", "world"]
   using_include.include?("hello")
+  using_include = ["good", "bye"]
+  using_include.include?("love")
 end
 
 def using_sort(array)
